47beebcec6295c486e09a1e9eaa51c162f71f617
p660710762 <phetsiri_n2@su.ac.th>
2025-09-17 08:53:40 +0000

    week7

---
+++ week7-lab2/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func getEnv(key, dafaultValue string) string {
+	if value := os.Getenv(key); value != "" {
+		return value
+	}
+	return dafaultValue
+}
+
+func main() {
+	//value := os.Getenv("DB_HOST")
+	host := getEnv("DB_HOST", "")
+	name := getEnv("DB_NAME", "")
+	user := getEnv("DB_USER", "")
+	password := getEnv("DB_PASSWORD", "")
+	port := getEnv("DB_PORT", "")
+
+	conSt := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s" , host, port , user, password, name)
+	fmt.Println(conSt)
+
+}
